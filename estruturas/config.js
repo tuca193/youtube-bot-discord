91d359e3e1024aa234c48a9b1dd8039e5ca45a8c
tuca193 <68290816+tuca193@users.noreply.github.com>
2020-11-27 10:44:05 -0300

    Update config.js

--- estruturas/config.js
+++ estruturas/config.js
@@ -1,13 +1,6 @@
 module.exports = {
-    token: 'NzMyNzkyMzI4ODU4NjMyMTky.Xw5wCQ.oRnxsl1tZ-rSK2BKTLjl6CfFuxM',
+    token: 'token',
     firebaseConfig: {
-        apiKey: "AIzaSyAlrGKd-GOQg3ziR6tBjjJJYw9m7kX63h0",
-        authDomain: "kaguya-18e68.firebaseapp.com",
-        databaseURL: "https://kaguya-18e68.firebaseio.com",
-        projectId: "kaguya-18e68",
-        storageBucket: "kaguya-18e68.appspot.com",
-        messagingSenderId: "640009018948",
-        appId: "1:640009018948:web:3182aae7594dcd81986611",
-        measurementId: "G-64HV87K6SJ"
+ 'firebaseconfig'
       }
 }
